5e8f3f1dbd392f51c0fcc6bfbffece6e30a1e072
chrisbcooper <coopercb@usc.edu>
2020-12-19 20:18:23 -0800

    Format side navbar

--- client/src/Components/Parts/Nav.js
+++ client/src/Components/Parts/Nav.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import '../../Styles/App.css';
 import {logout} from '../../Spotify';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faUser, faMusic, faHistory, faListUl, faSignOutAlt, faSpotify } from '@fortawesome/free-solid-svg-icons';
+import SpotifyIcon from '../../Images/SpotifyIcon.png';
 
 const NavHeader = () => {
 
     return (
         <nav className='sidenav'>
-            <a href='/'>Home</a>
-            <a href='/recently_played_songs'>Recent</a>
-            <a href='/top_songs'>Top Songs</a>
-            <a href='/top_artists'>Top Artists</a>
-            <a href='/current_playlists'>Playlists</a>
+                <div>
+                    <a href='/'><img class='spotify-icon' src={SpotifyIcon} alt=""/></a>
+                </div>
+                <div>
+                    <a href='/recently_played_songs'><div className='icon-div'><FontAwesomeIcon className='icon' icon={faHistory} /></div> Recent</a>
+                    <a href='/top_songs'><div className='icon-div'><FontAwesomeIcon className='icon' icon={faMusic} /></div>Top Songs</a>
+                    <a href='/top_artists'><div className='icon-div'><FontAwesomeIcon className='icon' icon={faUser} /></div>Top Artists</a>
+                    <a href='/current_playlists'><div className='icon-div'><FontAwesomeIcon className='icon' icon={faListUl} /></div>Playlists</a>
+                </div>
+                <div>
+                    <button onClick={logout}><div className='icon-div'><FontAwesomeIcon className='icon' icon={faSignOutAlt} /></div>Logout</button>
+                </div>
+            
         </nav>
     );
 

b68a37b6ea7fd48f5727c71e53de976f45da4b30
chrisbcooper <coopercb@usc.edu>
2021-08-27 08:33:54 -0700

    Add in redirect for router

--- server/Setters/credentials.js
+++ server/Setters/credentials.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const spotify = {
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: process.env.REDIRECT_URI || "http://localhost:5000/api/callback",
-    client_uri: process.env.CLIENT_URI || "http://localhost:3000"
-  };
-
+	client_id: process.env.CLIENT_ID,
+	client_secret: process.env.CLIENT_SECRET,
+	redirect_uri:
+		process.env.REDIRECT_URI || 'http://localhost:5000/api/callback',
+	client_uri: process.env.CLIENT_URI || 'http://localhost:3000',
+};
 module.exports = spotify;
